e145a78426192a674f3aec51f911135fc07f765d
Rifat8080 <123451855+Rifat8080@users.noreply.github.com>
2023-07-30 09:10:19 +0600

    Few changes

--- src/components/Public.js
+++ src/components/Public.js
@@ -1,28 +1,35 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 const Public = () => {
-    const content = (
-        <section className="public">
-            <header>
-                <h1>Welcome to <span className="nowrap">Dan D. Repairs!</span></h1>
-            </header>
-            <main className="public__main">
-                <p>Located in Beautiful Downtown Foo City, Dan D. Repairs  provides a trained staff ready to meet your tech repair needs.</p>
-                <address className="public__addr">
-                    Dan D. Repairs<br />
-                    555 Foo Drive<br />
-                    Foo City, CA 12345<br />
-                    <a href="tel:+15555555555">(555) 555-5555</a>
-                </address>
-                <br />
-                <p>Owner: Dan Davidson</p>
-            </main>
-            <footer>
-                <Link to="/login">Employee Login</Link>
-            </footer>
-        </section>
-
-    )
-    return content
-}
-export default Public
+  const content = (
+    <section className="public">
+      <header>
+        <h1>
+          Welcome to <span className="nowrap">JuubixNotes!</span>
+        </h1>
+      </header>
+      <main className="public__main">
+        <p>
+          Juubix, a new fintech start up that growing with it's talented hard
+          workers
+        </p>
+        <address className="public__addr">
+          Juubix
+          <br />
+          555 Foo Drive
+          <br />
+          Foo City, CA 12345
+          <br />
+          <a href="tel:+15555555555">(555) 555-5555</a>
+        </address>
+        <br />
+        <p>Owner: Juubix Fintech</p>
+      </main>
+      <footer>
+        <Link to="/login">Employee Login</Link>
+      </footer>
+    </section>
+  );
+  return content;
+};
+export default Public;
